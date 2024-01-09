be744198abbfc46c3724cf18f076d43e95298ed6
fulviavaz <fulviavaz@hotmail.com>
2024-01-09 16:57:31 -0300

    create data json

--- src/components/Produto/index.tsx
+++ src/components/Produto/index.tsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import styles from './Produto.module.scss'
-import teclado from '../../assets/product-image-teclado.jpg'
+import produtos from '../../data/produtos.json'
+
+
 
 export default function Produto() {
   return (
-    <div className={styles.produto}>
-      <img className={styles.produto__imagem} src={teclado} alt="teclado" />
-      <div className={styles.produto__info}>
-        <p className={styles.produto__info__nome}>
-          Teclado Gamer Mecânico Low Profile RGB AW510K 580
-        </p>
-        <div className={styles.produto__info__preco}>
-          <p className={styles.produto__info__preco__wrapp}>R$ 8.599,90</p>            
-            <div className={styles.produto__info__preco__botao}>-</div>
-            <p className={styles.produto__info__preco__numero}>1</p>
-            <div className={styles.produto__info__preco__botao}>+</div>          
-        </div>
+    <div className={styles.container}>
+      {produtos.map((produto) => (
+        <div className={styles.produto}>
+          <img className={styles.produto__imagem} src={produto.imagem} alt="teclado" />
+          <div className={styles.produto__info}>
+            <p className={styles.produto__info__nome}>
+              {produto.nome}
+            </p>
+            <div className={styles.produto__info__preco}>
+              <p className={styles.produto__info__preco__wrapp}>R$ {produto.preco}</p>
+              <div className={styles.produto__info__preco__botao}>-</div>
+              <p className={styles.produto__info__preco__numero}>1</p>
+              <div className={styles.produto__info__preco__botao}>+</div>
+            </div>
           </div>
-          
-      </div>        
-      
+        </div>
+      ))}
+    </div>
   );
 }
